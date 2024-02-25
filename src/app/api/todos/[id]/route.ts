5d00c75f2da4686b937adae74a5a12444705679d
JosueXA <josue.xicotencatl.a@gmail.com>
2024-02-25 13:26:52 -0600

    done created CRUD routes

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma';
+import { Todo } from '@prisma/client';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 interface Segments {
   params: {
@@ -7,7 +9,35 @@ interface Segments {
   }
 }
 
+const getTodo = async( id: string ): Promise<Todo | null> => {
+
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  return todo;
+}
+
 export async function GET(request: Request, { params }: Segments ) { 
+
+  const todo = await getTodo( params.id );
+
+  if ( !todo ) {
+    return NextResponse.json({
+      message: `Todo with id ${ params.id } not found`
+    },{
+      status: 404
+    })
+  }
+
+  return NextResponse.json(todo);
+  
+}
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional()
+})
+
+export async function PUT(request: Request, { params }: Segments ) { 
 
   const { id } = params || '';
 
@@ -23,15 +53,18 @@ export async function GET(request: Request, { params }: Segments ) {
     })
   }
 
-  return NextResponse.json(todo);
+  try {
+    const { description, complete } = await putSchema.validate( await request.json() );
   
-}
+    const updatedTodo = await prisma.todo.update({ 
+      where: { id }, 
+      data: { description, complete }
+    })
+  
+    return NextResponse.json(updatedTodo);
 
-
-export async function POST(request: Request) { 
-
-  const body = await request.json();
-
-  return NextResponse.json(body);
+  } catch (error) {
+    return NextResponse.json( error , { status: 400 } );
+  }
 
 }
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: Request) { 
 
@@ -28,6 +29,26 @@ export async function GET(request: Request) {
     skip: skip
   });
 
-  
   return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false)
+});
+
+export async function POST(request: Request) {
+
+  try {
+    const { description, complete } = await postSchema.validate( await request.json() ) ;
+  
+    const todo = await prisma.todo.create({ data: { description, complete } });
+  
+    return NextResponse.json(todo);
+
+  } catch (error) {
+    return NextResponse.json( error, { status: 400 } );
+  }
+
+
 }
