ddf4034bec230d0352623edf7eaeafa63039f0a0
JosueXA <josue.xicotencatl.a@gmail.com>
2024-02-26 12:27:24 -0600

    modify files

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -50,5 +50,17 @@ export async function POST(request: Request) {
     return NextResponse.json( error, { status: 400 } );
   }
 
+}
+
+export async function DELETE(request: Request ) {
+
+  try {
+    await prisma.todo.deleteMany({ where: { complete: true }} );
+
+    return NextResponse.json('Delete successfully');
+
+  } catch (error) {
+    return NextResponse.json( error, { status: 400 } );
+  }
 
 }
