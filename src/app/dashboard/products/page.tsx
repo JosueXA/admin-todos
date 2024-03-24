59eea33000a7123540fff4c13989a4d8d92ec702
JosueXA <josue.xicotencatl.a@gmail.com>
2024-03-24 16:50:20 -0600

    add products & cart components and funtional operations done

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,10 +1,15 @@
+import { products } from '@/products/data/products';
 import { ProductCard } from '../../../products/components/ProductCard';
 
 export default function ProductsPage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2">
       {/* Product Card */}
-      <ProductCard />
+      {
+        products.map( (product) => (
+          <ProductCard key={product.id} {...product}/>
+        ))
+      }
     </div>
   );
 }
