4614eaba10f9510f26ae33c46a3a6db9d1255203
JosueXA <josue.xicotencatl.a@gmail.com>
2024-04-02 18:24:25 -0600

    add auth github provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,27 @@
 import { WidgetItem } from "@/components";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect, RedirectType } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+
+  const session = await getServerSession( authOptions );
+
+  if( !session ) {
+    redirect('/api/auth/sigin');
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       
-      {/* <WidgetItem /> */}
+      <WidgetItem title="Usuario conectado S-Side" >
+        <div className="flex flex-col">
+          <span>{session.user?.name}</span>
+          <span>{session.user?.image}</span>
+          <span>{session.user?.email}</span>
+        </div>
+        
+      </WidgetItem>
 
     </div>  
   );
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link';
 import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
 import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from 'react-icons/io5';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
 const menuItems = [
   {
@@ -28,7 +31,14 @@ const menuItems = [
   }
 ]
 
-export const Sidebar = () => {
+export const Sidebar = async() => {
+
+  const session = await getServerSession( authOptions );
+
+  if( !session ) {
+    redirect('/api/auth/sigin');
+  }
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -44,13 +54,13 @@ export const Sidebar = () => {
         </div>
 
         <div className="mt-8 text-center">
-          <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+          <Image src={ String(session.user?.image) }
             width={150} 
             height={150} 
             alt="" 
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{ session.user?.name }</h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
 
